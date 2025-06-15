c137cecdbaf242dc9a0a3c1e6418b54be53a7855
Hardavan Alaie <h.alaie@gmx.de>
2025-06-15 20:49:19 +0200

    start with level 2

---
+++ levels/level2.js
@@ -0,0 +1,46 @@
+const level2 = new level2(
+  [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
+  [new Cloud()],
+  [
+    new BackgroundObject('img/5_background/layers/air.png', 0),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+    new BackgroundObject('img/5_background/layers/air.png', 719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+    new BackgroundObject('img/5_background/layers/air.png', -720),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -720),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -720),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -720),
+    new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719 * 2),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719 * 2),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
+    new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 * 3),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719 * 3),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
+  ],
+  [
+    new CollectableBottle(300, 350),
+    new CollectableBottle(350, 300),
+    new CollectableBottle(400, 300),
+    new CollectableBottle(350, 350),
+    new CollectableBottle(400, 350),
+    new CollectableBottle(450, 350),
+    new CollectableBottle(600, 350),
+    new CollectableBottle(900, 350)
+  ],
+  [
+    new CollectableCoin(100, 350),
+    new CollectableCoin(150, 300),
+    new CollectableCoin(200, 300),
+    new CollectableCoin(150, 350),
+    new CollectableCoin(200, 350),
+    new CollectableCoin(250, 350),
+    new CollectableCoin(400, 350),
+    new CollectableCoin(700, 350)
+  ]
+);
