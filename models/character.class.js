ae6110c748e5ffa48b1c207aef581780e2d3ab92
Hardavan Alaie <h.alaie@gmx.de>
2025-06-24 15:00:34 +0200

    changes the character img array

--- models/character.class.js
+++ models/character.class.js
@@ -3,10 +3,10 @@
   y = 90;
   speed = 10;
 
-  //   IMAGES_RUNNING = Array.from(
-  //     { length: 8 },
-  //     (_, i) => `img/2_character_pepe/2_walk/W-2${i + 1}.png`
-  //   );
+  //   IMAGES_RUNNING = Array.from({ length: 8 },(_, i) => `img/2_character_pepe/2_walk/W-2${i + 1}.png`);
+  //     
+  //     
+  //   
   IMAGES_RUNNING = [
     "img/2_character_pepe/2_walk/W-21.png",
     "img/2_character_pepe/2_walk/W-22.png",
@@ -15,6 +15,19 @@
     "img/2_character_pepe/2_walk/W-25.png",
     "img/2_character_pepe/2_walk/W-26.png",
   ];
+
+    // IMAGES_IDLE = [
+  //   "img/2_character_pepe/1_idle/idle/I-1.png",
+  //   "img/2_character_pepe/1_idle/idle/I-2.png",
+  //   "img/2_character_pepe/1_idle/idle/I-3.png",
+  //   "img/2_character_pepe/1_idle/idle/I-4.png",
+  //   "img/2_character_pepe/1_idle/idle/I-5.png",
+  //   "img/2_character_pepe/1_idle/idle/I-6.png",
+  //   "img/2_character_pepe/1_idle/idle/I-7.png",
+  //   "img/2_character_pepe/1_idle/idle/I-8.png",
+  //   "img/2_character_pepe/1_idle/idle/I-9.png",
+  //   "img/2_character_pepe/1_idle/idle/I-10.png",
+  // ];
 
   // IMAGES_JUMPING = Array.from(
   //   { length: 9 },
@@ -69,55 +82,11 @@ class Character extends MovableObject {
   otherDirection = false;
   isHurtTimer = false;
 
-  IMAGES_WALKING = [
-    "img/2_character_pepe/2_walk/W-21.png",
-    "img/2_character_pepe/2_walk/W-22.png",
-    "img/2_character_pepe/2_walk/W-23.png",
-    "img/2_character_pepe/2_walk/W-24.png",
-    "img/2_character_pepe/2_walk/W-25.png",
-    "img/2_character_pepe/2_walk/W-26.png",
-  ];
-
-  IMAGES_IDLE = [
-    "img/2_character_pepe/1_idle/idle/I-1.png",
-    "img/2_character_pepe/1_idle/idle/I-2.png",
-    "img/2_character_pepe/1_idle/idle/I-3.png",
-    "img/2_character_pepe/1_idle/idle/I-4.png",
-    "img/2_character_pepe/1_idle/idle/I-5.png",
-    "img/2_character_pepe/1_idle/idle/I-6.png",
-    "img/2_character_pepe/1_idle/idle/I-7.png",
-    "img/2_character_pepe/1_idle/idle/I-8.png",
-    "img/2_character_pepe/1_idle/idle/I-9.png",
-    "img/2_character_pepe/1_idle/idle/I-10.png",
-  ];
-
-  IMAGES_JUMPING = [
-    "img/2_character_pepe/3_jump/J-31.png",
-    "img/2_character_pepe/3_jump/J-32.png",
-    "img/2_character_pepe/3_jump/J-33.png",
-    "img/2_character_pepe/3_jump/J-34.png",
-    "img/2_character_pepe/3_jump/J-35.png",
-    "img/2_character_pepe/3_jump/J-36.png",
-    "img/2_character_pepe/3_jump/J-37.png",
-    "img/2_character_pepe/3_jump/J-38.png",
-    "img/2_character_pepe/3_jump/J-39.png",
-  ];
-
-  IMAGES_HURT = [
-    "img/2_character_pepe/4_hurt/H-41.png",
-    "img/2_character_pepe/4_hurt/H-42.png",
-    "img/2_character_pepe/4_hurt/H-43.png",
-  ];
-
-  IMAGES_DEAD = [
-    "img/2_character_pepe/5_dead/D-51.png",
-    "img/2_character_pepe/5_dead/D-52.png",
-    "img/2_character_pepe/5_dead/D-53.png",
-    "img/2_character_pepe/5_dead/D-54.png",
-    "img/2_character_pepe/5_dead/D-55.png",
-    "img/2_character_pepe/5_dead/D-56.png",
-    "img/2_character_pepe/5_dead/D-57.png",
-  ];
+  IMAGES_WALKING = Array.from({ length: 6 },(_, i) => `img/2_character_pepe/2_walk/W-2${i + 1}.png`);
+  IMAGES_IDLE = Array.from({ length: 10 },(_, i) => `img/2_character_pepe/1_idle/idle/I-${i + 1}.png`);
+  IMAGES_JUMPING = Array.from({ length: 9 },(_, i) => `img/2_character_pepe/3_jump/J-3${i + 1}.png`);
+  IMAGES_HURT = Array.from({ length: 3 },(_, i) => `img/2_character_pepe/4_hurt/H-4${i + 1}.png`);
+  IMAGES_DEAD = Array.from({ length: 7 },(_, i) => `img/2_character_pepe/5_dead/D-5${i + 1}.png`);
 
   constructor() {
     super().loadImage(this.IMAGES_WALKING[0]);
