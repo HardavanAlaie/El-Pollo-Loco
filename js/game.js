eb06138bb1d3670a075019f5fa3be8af60307928
Hardavan Alaie <h.alaie@gmx.de>
2025-06-26 19:44:36 +0200

    start button

--- js/game.js
+++ js/game.js
@@ -1,6 +1,63 @@
+// let canvas;
+// let world;
+// let keyboard = new Keyboard();
+
+// function init() {
+//   canvas = document.getElementById("canvas");
+//   world = new World(canvas, keyboard);
+// }
+
+// window.addEventListener("keydown", (e) => {
+//   if (e.keyCode == 37) {
+//     keyboard.LEFT = true;
+//   }
+//   if (e.keyCode == 38) {
+//     keyboard.UP = true;
+//   }
+//   if (e.keyCode == 39) {
+//     keyboard.RIGHT = true;
+//   }
+//   if (e.keyCode == 40) {
+//     keyboard.DOWN = true;
+//   }
+//   // if (e.keyCode == 32) {
+//   //   keyboard.SPACE = true;
+//   // }
+//   if (e.keyCode == 68) {
+//     keyboard.D = true;
+//   }
+// });
+
+// window.addEventListener("keyup", (e) => {
+//   if (e.keyCode == 37) {
+//     keyboard.LEFT = false;
+//   }
+//   if (e.keyCode == 38) {
+//     keyboard.UP = false;
+//   }
+//   if (e.keyCode == 39) {
+//     keyboard.RIGHT = false;
+//   }
+//   if (e.keyCode == 40) {
+//     keyboard.DOWN = false;
+//   }
+//   // if (e.keyCode == 32) {
+//   //   keyboard.SPACE = false;
+//   // }
+//   if (e.keyCode == 68) {
+//     keyboard.D = false;
+//   }
+// });
+
 let canvas;
 let world;
 let keyboard = new Keyboard();
+
+function startGame() {
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("canvas").style.display = "block";
+  init();
+}
 
 function init() {
   canvas = document.getElementById("canvas");
@@ -8,57 +65,31 @@ function init() {
 }
 
 window.addEventListener("keydown", (e) => {
-  if (e.keyCode == 37) {
-    keyboard.LEFT = true;
-  }
-  if (e.keyCode == 38) {
-    keyboard.UP = true;
-  }
-  if (e.keyCode == 39) {
-    keyboard.RIGHT = true;
-  }
-  if (e.keyCode == 40) {
-    keyboard.DOWN = true;
-  }
-  // if (e.keyCode == 32) {
-  //   keyboard.SPACE = true;
-  // }
-  if (e.keyCode == 68) {
-    keyboard.D = true;
-  }
+  if (e.keyCode == 37) keyboard.LEFT = true;
+  if (e.keyCode == 38) keyboard.UP = true;
+  if (e.keyCode == 39) keyboard.RIGHT = true;
+  if (e.keyCode == 40) keyboard.DOWN = true;
+  if (e.keyCode == 68) keyboard.D = true;
 });
 
 window.addEventListener("keyup", (e) => {
-  if (e.keyCode == 37) {
-    keyboard.LEFT = false;
-  }
-  if (e.keyCode == 38) {
-    keyboard.UP = false;
-  }
-  if (e.keyCode == 39) {
-    keyboard.RIGHT = false;
-  }
-  if (e.keyCode == 40) {
-    keyboard.DOWN = false;
-  }
-  // if (e.keyCode == 32) {
-  //   keyboard.SPACE = false;
-  // }
-  if (e.keyCode == 68) {
-    keyboard.D = false;
-  }
+  if (e.keyCode == 37) keyboard.LEFT = false;
+  if (e.keyCode == 38) keyboard.UP = false;
+  if (e.keyCode == 39) keyboard.RIGHT = false;
+  if (e.keyCode == 40) keyboard.DOWN = false;
+  if (e.keyCode == 68) keyboard.D = false;
 });
 
-// window.addEventListener("keydown", (e) => {
-//   if (e.code === "ArrowRight") keyboard.RIGHT = true;
-//   if (e.code === "ArrowLeft") keyboard.LEFT = true;
-//   if (e.code === "Space") keyboard.SPACE = true;
-//   if (e.code === "KeyD") keyboard.D = true;
-// });
+// // window.addEventListener("keydown", (e) => {
+// //   if (e.code === "ArrowRight") keyboard.RIGHT = true;
+// //   if (e.code === "ArrowLeft") keyboard.LEFT = true;
+// //   if (e.code === "Space") keyboard.SPACE = true;
+// //   if (e.code === "KeyD") keyboard.D = true;
+// // });
 
-// window.addEventListener("keyup", (e) => {
-//   if (e.code === "ArrowRight") keyboard.RIGHT = false;
-//   if (e.code === "ArrowLeft") keyboard.LEFT = false;
-//   if (e.code === "Space") keyboard.SPACE = false;
-//   if (e.code === "KeyD") keyboard.D = false;
-// });
+// // window.addEventListener("keyup", (e) => {
+// //   if (e.code === "ArrowRight") keyboard.RIGHT = false;
+// //   if (e.code === "ArrowLeft") keyboard.LEFT = false;
+// //   if (e.code === "Space") keyboard.SPACE = false;
+// //   if (e.code === "KeyD") keyboard.D = false;
+// // });
