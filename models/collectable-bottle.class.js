b0d9aab94f68b0c6b1fca25395693cf540069e0e
Hardavan Alaie <h.alaie@gmx.de>
2025-06-29 20:47:50 +0200

    animated bottles and coins

--- models/collectable-bottle.class.js
+++ models/collectable-bottle.class.js
@@ -1,9 +1,23 @@
 class CollectableBottle extends MovableObject {
+  IMAGES = [
+    "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
+    "img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
+  ];
+
   constructor(x, y) {
-    super().loadImage("img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
+    super().loadImage(this.IMAGES[0]);
+    this.loadImages(this.IMAGES);
     this.x = x;
     this.y = y;
     this.width = 50;
     this.height = 60;
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES);
+    }, 250);
   }
 }
--- models/collectable-coin.class.js
+++ models/collectable-coin.class.js
@@ -1,9 +1,22 @@
 class CollectableCoin extends MovableObject {
+  IMAGES = [
+    "img/8_coin/coin_1.png", 
+    "img/8_coin/coin_2.png"
+  ];
+
   constructor(x, y) {
-    super().loadImage("img/8_coin/coin_1.png");
+    super().loadImage(this.IMAGES[0]);
+    this.loadImages(this.IMAGES);
     this.x = x;
     this.y = y;
     this.width = 50;
     this.height = 60;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES);
+    }, 200);
   }
 }
