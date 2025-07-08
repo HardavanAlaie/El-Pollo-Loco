5089eca6c4384943fc30cd018290d39d81a72840
Hardavan Alaie <h.alaie@gmx.de>
2025-07-08 21:24:18 +0200

    new spawn enemies

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,5 @@
 const level1 = new Level(
-  [new ChickenSmall(), new EndbossLevel1()],
+  [new ChickenSmall(), new ChickenSmall(), new ChickenSmall(), new EndbossLevel1()],
   [new Cloud(), new Cloud(), new Cloud()],
   [
     new BackgroundObject("img/5_background/layers/air.png", 0),
