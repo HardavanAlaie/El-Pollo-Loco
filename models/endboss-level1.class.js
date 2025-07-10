a600047ecafe95a3392c03ed04363292689354fb
Hardavan Alaie <h.alaie@gmx.de>
2025-07-10 22:05:53 +0200

    new animation images for endboss

--- models/endboss-level1.class.js
+++ models/endboss-level1.class.js
@@ -15,6 +15,29 @@ class EndbossLevel1 extends MovableObject {
     "img/4_enemie_boss_chicken/2_alert/G12.png",
   ];
 
+  IMAGES_ATTACK = [
+    "img/4_enemie_boss_chicken/3_attack/G13.png",
+    "img/4_enemie_boss_chicken/1_attack/G14.png",
+    "img/4_enemie_boss_chicken/1_attack/G15.png",
+    "img/4_enemie_boss_chicken/1_attack/G16.png",
+    "img/4_enemie_boss_chicken/1_attack/G17.png",
+    "img/4_enemie_boss_chicken/1_attack/G18.png",
+    "img/4_enemie_boss_chicken/1_attack/G19.png",
+    "img/4_enemie_boss_chicken/1_attack/G20.png",
+  ];
+
+  IMAGES_HURT = [
+    "img/4_enemie_boss_chicken/4_hurt/G21.png",
+    "img/4_enemie_boss_chicken/4_hurt/G22.png",
+    "img/4_enemie_boss_chicken/4_hurt/G23.png",
+  ];
+
+  IMAGES_DEAD = [
+    "img/4_enemie_boss_chicken/5_dead/G24.png",
+    "img/4_enemie_boss_chicken/5_dead/G25.png",
+    "img/4_enemie_boss_chicken/3_dead/G26.png",
+  ];
+
   constructor() {
     super().loadImage(this.IMAGES_ALERT[0]);
     this.loadImages(this.IMAGES_ALERT);
