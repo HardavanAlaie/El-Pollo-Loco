cde05e8b324d1f48b72d3033d6585742e62068e5
Hardavan Alaie <h.alaie@gmx.de>
2025-07-26 17:28:31 +0200

    new images chiceknNormal "dead"

--- models/chicken-normal.class.js
+++ models/chicken-normal.class.js
@@ -4,6 +4,10 @@ class ChickenNormal extends MovableObject {
     "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
     "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
   ];
+  IMAGES_DEAD = [
+    "img/3_enemies_chicken/chicken_normal/2_dead/dead.png",
+  ];
+  
   constructor() {
     super().loadImage(this.IMAGES_RUNNING[0]);
     this.loadImages(this.IMAGES_RUNNING);
