6e17e977cb7201df7c7dc904eca00598e3ed8ca6
Hardavan Alaie <h.alaie@gmx.de>
2025-08-05 19:47:02 +0200

    remove the parallaxFactor for bg

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,16 +1,16 @@
-// class BackgroundObject extends MovableObject {
-//   //y = 0;
-//   width = 720;
-//   height = 480;
+class BackgroundObject extends MovableObject {
+  //y = 0;
+  width = 720;
+  height = 480;
 
-//   constructor(imagePath, x) {
-//     super().loadImage(imagePath);
-//     this.x = x;
-//     this.y = 480 - this.height;
+  constructor(imagePath, x) {
+    super().loadImage(imagePath);
+    this.x = x;
+    this.y = 480 - this.height;
 
-//     //this.x = 10 + Math.random() * 500;
-//   }
-// }
+    //this.x = 10 + Math.random() * 500;
+  }
+}
 
 // class BackgroundObject extends DrawableObject {
 //   constructor(imagePath, x, parallaxFactor = 1.0) {
@@ -34,34 +34,34 @@
 //   }
 // }
 
-class BackgroundObject extends MovableObject {
-  width = 720;
-  height = 480;
+// class BackgroundObject extends MovableObject {
+//   width = 720;
+//   height = 480;
 
-  /**
-   * @param {string} imagePath - Der Pfad zum Bild
-   * @param {number} x - Die X-Position im Level
-   * @param {number} parallaxFactor - Wie stark sich das Bild beim Scrollen verschiebt (0 = ganz hinten, 1 = ganz vorne)
-   */
-  constructor(imagePath, x, parallaxFactor = 1.0) {
-    super().loadImage(imagePath);
-    this.x = x;
-    this.y = 480 - this.height; // Bodenhöhe
-    this.parallaxFactor = parallaxFactor;
-  }
+//   /**
+//    * @param {string} imagePath - Der Pfad zum Bild
+//    * @param {number} x - Die X-Position im Level
+//    * @param {number} parallaxFactor - Wie stark sich das Bild beim Scrollen verschiebt (0 = ganz hinten, 1 = ganz vorne)
+//    */
+//   constructor(imagePath, x, parallaxFactor = 1.0) {
+//     super().loadImage(imagePath);
+//     this.x = x;
+//     this.y = 480 - this.height; // Bodenhöhe
+//     this.parallaxFactor = parallaxFactor;
+//   }
 
-  /**
-   * Zeichnet das Hintergrundbild mit Parallax-Effekt
-   * @param {CanvasRenderingContext2D} ctx
-   * @param {number} camera_x
-   */
-  draw(ctx, camera_x) {
-    ctx.drawImage(
-      this.img,
-      this.x - camera_x * this.parallaxFactor,
-      this.y,
-      this.width,
-      this.height
-    );
-  }
-}
+//   /**
+//    * Zeichnet das Hintergrundbild mit Parallax-Effekt
+//    * @param {CanvasRenderingContext2D} ctx
+//    * @param {number} camera_x
+//    */
+//   draw(ctx, camera_x) {
+//     ctx.drawImage(
+//       this.img,
+//       this.x - camera_x * this.parallaxFactor,
+//       this.y,
+//       this.width,
+//       this.height
+//     );
+//   }
+// }
--- models/world.class.js
+++ models/world.class.js
@@ -700,10 +700,10 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.translate(this.camera_x, 0);
 
-    //this.addObjectsToMap(this.level.backgroundObjects || []);
-    (this.level.backgroundObjects || []).forEach((obj) => {
-      obj.draw(this.ctx, this.camera_x);
-    });
+    this.addObjectsToMap(this.level.backgroundObjects || []);
+    // (this.level.backgroundObjects || []).forEach((obj) => {
+    //   obj.draw(this.ctx, this.camera_x);
+    // });
 
     this.ctx.translate(-this.camera_x, 0);
 
