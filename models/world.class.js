d147cce72452238b0891daca4c1386bfd5d42e59
Hardavan Alaie <h.alaie@gmx.de>
2025-08-09 19:34:47 +0200

    in characterColliding() more area

--- models/world.class.js
+++ models/world.class.js
@@ -207,7 +207,8 @@ class World {
     if (this.character.isColliding(enemy)) {
       const characterBottom = this.character.y + this.character.height;
       const characterVerticalSpeed = this.character.speedY;
-      const enemyTop = enemy.y + enemy.height * 0.3;
+      //const enemyTop = enemy.y + enemy.height * 0.3;
+      const enemyTop = enemy.y + enemy.height * 0.5;
 
       const isAboveEnemy =
         characterBottom <= enemyTop + 10 && characterVerticalSpeed >= 0;
