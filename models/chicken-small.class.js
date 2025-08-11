7c393e93ae4bac7c4fcf28942b1fc918cce928c0
Hardavan Alaie <h.alaie@gmx.de>
2025-08-11 20:01:24 +0200

    remove the new class chickensmall and use old version

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -108,3 +108,72 @@ animate() {
     clearInterval(this.animationInterval);
   }
 }
+
+
+
+// class ChickenSmall extends MovableObject {
+//   IMAGES_RUNNING = [
+//     "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+//     "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+//     "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+//   ];
+//   IMAGES_DEAD = ["img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
+
+//   constructor() {
+//     super().loadImage(this.IMAGES_RUNNING[0]);
+//     this.loadImages(this.IMAGES_RUNNING);
+//     this.loadImages(this.IMAGES_DEAD);
+
+//     this.x = 500 + Math.random() * 2000;
+//     this.y = 380;
+//     this.width = 50;
+//     this.height = 50;
+//     this.speed = 0.3 + Math.random() * 0.5;
+//     this.energy = 100;
+//     this.dead = false;
+
+//     this.statusBar = new StatusBarEnemy(this);
+//     this.animate();
+//   }
+
+//   hit() {
+//     if (this.dead) return; // schon tot → nichts mehr machen
+
+//     this.energy -= 100;
+//     this.energy = Math.max(this.energy, 0);
+//     this.statusBar.setPercentage(this.energy);
+
+//     if (this.isDead()) {
+//       this.die();
+//     }
+//   }
+
+//   isDead() {
+//     return this.energy <= 0;
+//   }
+
+//   die() {
+//     this.dead = true;
+//     this.loadImage(this.IMAGES_DEAD[0]); // Dead-Bild setzen
+//     this.speed = 0; // nicht mehr bewegen
+
+//     // Nach 1 Sekunde Gegner entfernen
+//     setTimeout(() => {
+//       if (this.world) {
+//         const index = this.world.level.enemies.indexOf(this);
+//         if (index > -1) {
+//           this.world.level.enemies.splice(index, 1);
+//         }
+//       }
+//     }, 1000);
+//   }
+
+//   animate() {
+//     this.chickenAnimationInterval = setInterval(() => {
+//       if (!this.dead) {
+//         this.playAnimation(this.IMAGES_RUNNING);
+//         this.moveLeft();
+//       }
+//     }, 200);
+//   }
+// }
