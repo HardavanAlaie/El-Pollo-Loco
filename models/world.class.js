9bcdbb3d992660ea3b81ff6552ca71dc8a047667
Hardavan Alaie <h.alaie@gmx.de>
2025-08-13 09:15:43 +0200

    remove new version ifEnemyIsDead() function

--- models/world.class.js
+++ models/world.class.js
@@ -238,34 +238,34 @@ class World {
     }
   }
 
-  // ifEnemyIsDead(enemy) {
-  //   if (enemy.isDead?.()) {
-  //     if (enemy instanceof EndbossLevel1 || enemy instanceof EndbossLevel2) {
-  //       enemy.isMarkedDead = true;
-  //     } else {
-  //       const i = this.level.enemies.indexOf(enemy);
-  //       if (i >= 0) this.level.enemies.splice(i, 1);
-  //     }
-  //   }
-  // }
   ifEnemyIsDead(enemy) {
     if (enemy.isDead?.()) {
-        if (enemy instanceof EndbossLevel1 || enemy instanceof EndbossLevel2) {
-            enemy.isMarkedDead = true;
-        } else {
-            // Dead-Animation/Bild setzen
-            enemy.die?.();
-
-            // Gegner erst nach 2 Sekunden entfernen
-            setTimeout(() => {
-                const i = this.level.enemies.indexOf(enemy);
-                if (i >= 0) {
-                    this.level.enemies.splice(i, 1);
-                }
-            }, 2000); // Zeit anpassen, je nach gewünschter Dauer
-        }
+      if (enemy instanceof EndbossLevel1 || enemy instanceof EndbossLevel2) {
+        enemy.isMarkedDead = true;
+      } else {
+        const i = this.level.enemies.indexOf(enemy);
+        if (i >= 0) this.level.enemies.splice(i, 1);
+      }
     }
-}
+  }
+//   ifEnemyIsDead(enemy) {
+//     if (enemy.isDead?.()) {
+//         if (enemy instanceof EndbossLevel1 || enemy instanceof EndbossLevel2) {
+//             enemy.isMarkedDead = true;
+//         } else {
+//             // Dead-Animation/Bild setzen
+//             enemy.die?.();
+
+//             // Gegner erst nach 2 Sekunden entfernen
+//             setTimeout(() => {
+//                 const i = this.level.enemies.indexOf(enemy);
+//                 if (i >= 0) {
+//                     this.level.enemies.splice(i, 1);
+//                 }
+//             }, 2000); // Zeit anpassen, je nach gewünschter Dauer
+//         }
+//     }
+// }
 
 
   endGame() {
