baed7fd8c6cb704a0cb0f85e89cc96fbfca2cf0d
Hardavan Alaie <h.alaie@gmx.de>
2025-08-17 11:00:08 +0200

    new chickennormal version with start()

--- models/chicken-normal.class.js
+++ models/chicken-normal.class.js
@@ -1,23 +1,112 @@
+// class ChickenNormal extends MovableObject {
+//   IMAGES_RUNNING = [
+//     "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+//     "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+//     "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+//   ];
+//   IMAGES_DEAD = ["img/3_enemies_chicken/chicken_normal/2_dead/dead.png"];
+
+//   constructor() {
+//     super().loadImage(this.IMAGES_RUNNING[0]);
+//     this.loadImages(this.IMAGES_RUNNING);
+//     this.loadImages(this.IMAGES_DEAD);
+//     this.x = 500 + Math.random() * 2000;
+//     this.y = 355;
+//     this.width = 60;
+//     this.height = 60;
+//     this.speed = 0.3 + Math.random() * 0.5;
+//     this.energy = 100;
+//     this.statusBar = new StatusBarEnemy(this);
+//     this.animate();
+//   }
+
+//   hit() {
+//     this.energy -= 100;
+//     this.energy = Math.max(this.energy, 0);
+//     this.statusBar.setPercentage(this.energy);
+//   }
+//   // hit() {
+//   //   if (this.isDead()) return;
+
+//   //   this.energy -= 20;
+//   //   this.energy = Math.max(this.energy, 0);
+//   //   this.statusBar.setPercentage(this.energy);
+
+//   //   if (this.isDead()) {
+//   //     this.die();
+//   //   }
+//   // }
+
+//   // die() {
+//   //   this.playAnimation(this.IMAGES_DEAD);
+//   //   clearInterval(this.chicekAnimationInterval);
+//   // }
+
+//   isDead() {
+//     return this.energy <= 0;
+//   }
+
+//   animate() {
+//     setInterval(() => {
+//       this.moveLeft();
+//     }, 1000 / 60);
+
+//     setInterval(() => {
+//       this.playAnimation(this.IMAGES_RUNNING);
+//     }, 100);
+//   }
+//   //   animate() {
+//   //   this.chicekAnimationInterval = setInterval(() => {
+//   //     if (this.isDead()) {
+//   //       this.playAnimation(this.IMAGES_DEAD);
+//   //       clearInterval(this.chicekAnimationInterval);
+//   //     } else {
+//   //       this.playAnimation(this.IMAGES_RUNNING);
+//   //     }
+//   //   }, 100);
+//   // }
+
+//   //   start() {
+//   //   this.moveInterval = setInterval(() => this.moveLeft(), 1000 / 60);
+//   //   this.animationInterval = setInterval(
+//   //     () => this.playAnimation(this.IMAGES_RUNNING),
+//   //     100
+//   //   );
+//   // }
+
+//   // stop() {
+//   //   clearInterval(this.moveInterval);
+//   //   clearInterval(this.animationInterval);
+//   // }
+// }
+
+//---------------------------------------------------------------------------
+
 class ChickenNormal extends MovableObject {
   IMAGES_RUNNING = [
     "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
     "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
     "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
   ];
+
   IMAGES_DEAD = ["img/3_enemies_chicken/chicken_normal/2_dead/dead.png"];
 
   constructor() {
     super().loadImage(this.IMAGES_RUNNING[0]);
     this.loadImages(this.IMAGES_RUNNING);
     this.loadImages(this.IMAGES_DEAD);
+
     this.x = 500 + Math.random() * 2000;
-    this.y = 355;
-    this.width = 60;
-    this.height = 60;
-    this.speed = 0.3 + Math.random() * 0.5;
+    this.y = 360; // etwas tiefer als small chickens
+    this.width = 70;
+    this.height = 70;
+    this.speed = 0.2 + Math.random() * 0.4;
     this.energy = 100;
+    this.dead = false;
+
     this.statusBar = new StatusBarEnemy(this);
-    this.animate();
+
+    this.start(); // 👈 startet automatisch
   }
 
   hit() {
@@ -25,22 +114,6 @@ class ChickenNormal extends MovableObject {
     this.energy = Math.max(this.energy, 0);
     this.statusBar.setPercentage(this.energy);
   }
-  // hit() {
-  //   if (this.isDead()) return;
-
-  //   this.energy -= 20;
-  //   this.energy = Math.max(this.energy, 0);
-  //   this.statusBar.setPercentage(this.energy);
-
-  //   if (this.isDead()) {
-  //     this.die();
-  //   }
-  // }
-
-  // die() {
-  //   this.playAnimation(this.IMAGES_DEAD);
-  //   clearInterval(this.chicekAnimationInterval);
-  // }
 
   isDead() {
     return this.energy <= 0;
@@ -55,27 +128,15 @@ class ChickenNormal extends MovableObject {
       this.playAnimation(this.IMAGES_RUNNING);
     }, 100);
   }
-  //   animate() {
-  //   this.chicekAnimationInterval = setInterval(() => {
-  //     if (this.isDead()) {
-  //       this.playAnimation(this.IMAGES_DEAD);
-  //       clearInterval(this.chicekAnimationInterval);
-  //     } else {
-  //       this.playAnimation(this.IMAGES_RUNNING);
-  //     }
-  //   }, 100);
-  // }
 
-  //   start() {
-  //   this.moveInterval = setInterval(() => this.moveLeft(), 1000 / 60);
-  //   this.animationInterval = setInterval(
-  //     () => this.playAnimation(this.IMAGES_RUNNING),
-  //     100
-  //   );
-  // }
+  start() {
+    if (!this.chickenAnimationInterval) {
+      this.animate();
+    }
+  }
 
-  // stop() {
-  //   clearInterval(this.moveInterval);
-  //   clearInterval(this.animationInterval);
-  // }
+  stop() {
+    clearInterval(this.chickenAnimationInterval);
+  }
 }
+
