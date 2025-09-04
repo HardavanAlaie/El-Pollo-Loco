3def0d94de8d8108c061db4cb30915ee4f49c35c
Hardavan Alaie <h.alaie@gmx.de>
2025-09-04 19:49:35 +0200

    new version level1.js

--- models/level.class.js
+++ models/level.class.js
@@ -1,23 +1,49 @@
+// class Level {
+//   enemies;
+//   clouds;
+//   backgroundObjects;
+//   level_end_x = 2250;
+
+//   collectableObjects = [];
+//   collectableCoins = [];
+
+//   constructor(
+//     enemies,
+//     clouds,
+//     backgroundObjects,
+//     collectableObjects,
+//     collectableCoins
+//   ) {
+//     this.enemies = enemies;
+//     this.clouds = clouds;
+//     this.backgroundObjects = backgroundObjects;
+//     this.collectableObjects = collectableObjects;
+//     this.collectableCoins = collectableCoins;
+//   }
+// }
+
+
 class Level {
   enemies;
   clouds;
   backgroundObjects;
-  level_end_x = 2250;
-
   collectableObjects = [];
   collectableCoins = [];
+  level_end_x;
 
   constructor(
     enemies,
     clouds,
     backgroundObjects,
     collectableObjects,
-    collectableCoins
+    collectableCoins,
+    level_end_x = 2250 // Standard: 2250 px
   ) {
     this.enemies = enemies;
     this.clouds = clouds;
     this.backgroundObjects = backgroundObjects;
     this.collectableObjects = collectableObjects;
     this.collectableCoins = collectableCoins;
+    this.level_end_x = level_end_x;
   }
 }
