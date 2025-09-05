e7d418248579a4da7fea51624f318d5ba2450c6d
Hardavan Alaie <h.alaie@gmx.de>
2025-09-05 20:01:34 +0200

    remover the new version level class

--- models/level.class.js
+++ models/level.class.js
@@ -1,45 +1,45 @@
-// class Level {
-//   enemies;
-//   clouds;
-//   backgroundObjects;
-//   level_end_x = 2250;
-
-//   collectableObjects = [];
-//   collectableCoins = [];
-
-//   constructor(
-//     enemies,
-//     clouds,
-//     backgroundObjects,
-//     collectableObjects,
-//     collectableCoins
-//   ) {
-//     this.enemies = enemies;
-//     this.clouds = clouds;
-//     this.backgroundObjects = backgroundObjects;
-//     this.collectableObjects = collectableObjects;
-//     this.collectableCoins = collectableCoins;
-//   }
-// }
-
-
 class Level {
   enemies;
   clouds;
   backgroundObjects;
+  level_end_x = 2250;
+
   collectableObjects = [];
   collectableCoins = [];
-  level_end_x;
 
-  constructor(enemies, clouds, backgroundObjects, collectableObjects, collectableCoins, level_end_x = 3000) {
+  constructor(
+    enemies,
+    clouds,
+    backgroundObjects,
+    collectableObjects,
+    collectableCoins
+  ) {
     this.enemies = enemies;
     this.clouds = clouds;
     this.backgroundObjects = backgroundObjects;
     this.collectableObjects = collectableObjects;
     this.collectableCoins = collectableCoins;
-    this.level_end_x = level_end_x; // nur ein Level-Ende
   }
 }
+
+
+// class Level {
+//   enemies;
+//   clouds;
+//   backgroundObjects;
+//   collectableObjects = [];
+//   collectableCoins = [];
+//   level_end_x;
+
+//   constructor(enemies, clouds, backgroundObjects, collectableObjects, collectableCoins, level_end_x = 3000) {
+//     this.enemies = enemies;
+//     this.clouds = clouds;
+//     this.backgroundObjects = backgroundObjects;
+//     this.collectableObjects = collectableObjects;
+//     this.collectableCoins = collectableCoins;
+//     this.level_end_x = level_end_x; // nur ein Level-Ende
+//   }
+// }
 
 
 
