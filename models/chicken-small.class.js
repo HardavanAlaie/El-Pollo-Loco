acc7b8435fbf49754e45c6d2b1916e77984fc368
Hardavan Alaie <h.alaie@gmx.de>
2025-10-09 20:18:32 +0200

    new version chicken-small class

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -1,3 +1,79 @@
+// class ChickenSmall extends MovableObject {
+//   IMAGES_RUNNING = [
+//     "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+//     "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+//     "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+//   ];
+
+//   IMAGES_DEAD = ["img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
+
+//   constructor() {
+//     super().loadImage(this.IMAGES_RUNNING[0]);
+//     this.loadImages(this.IMAGES_RUNNING);
+//     this.loadImages(this.IMAGES_DEAD);
+
+//     this.x = 500 + Math.random() * 2000;
+//     this.y = 380;
+//     this.width = 50;
+//     this.height = 50;
+//     this.speed = 0.3 + Math.random() * 0.5;
+//     this.energy = 100;
+//     this.dead = false;
+//     this.statusBar = new StatusBarEnemy(this);
+//     this.start();
+//   }
+
+//   hit() {
+//     if (this.dead) return;
+//     this.energy -= 100;
+//     this.energy = Math.max(this.energy, 0);
+//     this.statusBar.setPercentage(this.energy);
+
+//     if (this.isDead()) {
+//       this.die();
+//     }
+//   }
+
+//   isDead() {
+//     return this.energy <= 0;
+//   }
+
+//   die() {
+//     this.dead = true;
+//     this.loadImage(this.IMAGES_DEAD[0]);
+//     this.speed = 0;
+//     this.stop();
+
+//     setTimeout(() => {
+//       const i = world.level.enemies.indexOf(this);
+//       if (i >= 0) {
+//         world.level.enemies.splice(i, 1);
+//       }
+//     }, 1000);
+//   }
+
+//   animate() {
+//     this.moveInterval = setInterval(() => {
+//       if (!this.dead) this.moveLeft();
+//     }, 1000 / 60);
+
+//     this.runInterval = setInterval(() => {
+//       if (!this.dead) this.playAnimation(this.IMAGES_RUNNING);
+//     }, 100);
+//   }
+
+//   start() {
+//     if (!this.chickenAnimationInterval) {
+//       this.animate();
+//     }
+//   }
+
+//   stop() {
+//     clearInterval(this.moveInterval);
+//     clearInterval(this.runInterval);
+//   }
+// }
+
 class ChickenSmall extends MovableObject {
   IMAGES_RUNNING = [
     "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
@@ -12,6 +88,7 @@ class ChickenSmall extends MovableObject {
     this.loadImages(this.IMAGES_RUNNING);
     this.loadImages(this.IMAGES_DEAD);
 
+    // 🐥 Attribute
     this.x = 500 + Math.random() * 2000;
     this.y = 380;
     this.width = 50;
@@ -19,39 +96,41 @@ class ChickenSmall extends MovableObject {
     this.speed = 0.3 + Math.random() * 0.5;
     this.energy = 100;
     this.dead = false;
+
+    // 💬 Gegner-Statusbalken
     this.statusBar = new StatusBarEnemy(this);
+
     this.start();
   }
 
+  /** 🩸 Wird getroffen */
   hit() {
     if (this.dead) return;
-    this.energy -= 100;
-    this.energy = Math.max(this.energy, 0);
+    this.energy = Math.max(this.energy - 100, 0);
     this.statusBar.setPercentage(this.energy);
-
-    if (this.isDead()) {
-      this.die();
-    }
+    if (this.isDead()) this.die();
   }
 
+  /** 💀 Prüfen ob tot */
   isDead() {
     return this.energy <= 0;
   }
 
+  /** ☠️ Todesanimation & Entfernen */
   die() {
     this.dead = true;
     this.loadImage(this.IMAGES_DEAD[0]);
     this.speed = 0;
     this.stop();
 
+    // Entferne Huhn aus der Level-Liste nach kurzer Zeit
     setTimeout(() => {
-      const i = world.level.enemies.indexOf(this);
-      if (i >= 0) {
-        world.level.enemies.splice(i, 1);
-      }
+      const index = world?.level?.enemies?.indexOf(this);
+      if (index >= 0) world.level.enemies.splice(index, 1);
     }, 1000);
   }
 
+  /** 🏃 Bewegungs- & Laufanimation */
   animate() {
     this.moveInterval = setInterval(() => {
       if (!this.dead) this.moveLeft();
@@ -62,12 +141,12 @@ class ChickenSmall extends MovableObject {
     }, 100);
   }
 
+  /** ▶️ Startet das Huhn (einmalig) */
   start() {
-    if (!this.chickenAnimationInterval) {
-      this.animate();
-    }
+    if (!this.chickenAnimationInterval) this.animate();
   }
 
+  /** ⏹️ Stoppt Bewegung & Animation */
   stop() {
     clearInterval(this.moveInterval);
     clearInterval(this.runInterval);
