ca75dc9e701fad1ca980ded090254d5f1604165e
Hardavan Alaie <h.alaie@gmx.de>
2025-10-14 19:48:32 +0200

    comment cloud class

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -3,11 +3,20 @@ class Cloud extends MovableObject {
   width = 500;
   height = 250;
 
+  /**
+   * Array of image paths representing cloud layer graphics.
+   * @type {string[]}
+   */
   IMAGES = [
     "img/5_background/layers/4_clouds/1.png",
     "img/5_background/layers/4_clouds/2.png",
   ];
 
+  /**
+   * Creates an instance of the Cloud class.
+   * Initializes the cloud with a random horizontal position and starts its animation.
+   * Loads the initial image for the cloud.
+   */
   constructor() {
     super().loadImage(this.IMAGES[0]);
 
@@ -15,6 +24,11 @@ class Cloud extends MovableObject {
     this.animate();
   }
 
+  /**
+   * Starts the animation for the cloud object by continuously moving it to the left.
+   * Decreases the `x` position by 0.15 units approximately every 1/60th of a second.
+   * Intended to create a smooth horizontal movement effect.
+   */
   animate() {
     setInterval(() => {
       this.x -= 0.15;
