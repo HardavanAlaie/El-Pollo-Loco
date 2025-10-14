1dfd093432ec38be4f1296ffd1ab6f4eb3362c5c
Hardavan Alaie <h.alaie@gmx.de>
2025-10-14 19:41:34 +0200

    comment bg-object class

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -2,6 +2,13 @@ class BackgroundObject extends MovableObject {
   width = 720;
   height = 480;
 
+  /**
+   * Creates an instance of the background object.
+   * Loads the image from the specified path and sets the x and y coordinates.
+   *
+   * @param {string} imagePath - The path to the image file.
+   * @param {number} x - The x-coordinate where the background object should be placed.
+   */
   constructor(imagePath, x) {
     super().loadImage(imagePath);
     this.x = x;
