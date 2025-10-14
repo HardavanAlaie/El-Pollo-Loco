62a80c31906611560b4d39166f71dd4b9f8225ad
Hardavan Alaie <h.alaie@gmx.de>
2025-10-14 19:50:38 +0200

    comment collectable bottle class

--- models/collectable-bottle.class.js
+++ models/collectable-bottle.class.js
@@ -1,9 +1,21 @@
 class CollectableBottle extends MovableObject {
+  /**
+   * Array of image paths representing the different states of the salsa bottle collectable.
+   * @type {string[]}
+   */
   IMAGES = [
     "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
     "img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
   ];
 
+  /**
+   * Creates an instance of the collectable bottle at the specified coordinates.
+   * Loads the initial image and all images for the bottle, sets its position and size,
+   * and starts the animation.
+   *
+   * @param {number} x - The x-coordinate of the bottle.
+   * @param {number} y - The y-coordinate of the bottle.
+   */
   constructor(x, y) {
     super().loadImage(this.IMAGES[0]);
     this.loadImages(this.IMAGES);
@@ -11,10 +23,14 @@ class CollectableBottle extends MovableObject {
     this.y = y;
     this.width = 50;
     this.height = 60;
-
     this.animate();
   }
 
+  /**
+   * Starts an animation loop that cycles through the object's images.
+   * Calls the playAnimation method every 250 milliseconds using the IMAGES array.
+   * Intended to animate the collectable bottle sprite.
+   */
   animate() {
     setInterval(() => {
       this.playAnimation(this.IMAGES);
