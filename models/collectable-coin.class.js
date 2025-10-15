91b7d4bb6033116d7629296c5e713f09fe530725
Hardavan Alaie <h.alaie@gmx.de>
2025-10-15 17:43:52 +0200

    comment collectable class

--- models/collectable-coin.class.js
+++ models/collectable-coin.class.js
@@ -1,6 +1,13 @@
 class CollectableCoin extends MovableObject {
   IMAGES = ["img/8_coin/coin_1.png", "img/8_coin/coin_2.png"];
 
+  /**
+   * Creates an instance of the collectable coin at the specified coordinates.
+   * Loads the initial image and all animation frames, sets position and size, and starts animation.
+   *
+   * @param {number} x - The x-coordinate of the coin.
+   * @param {number} y - The y-coordinate of the coin.
+   */
   constructor(x, y) {
     super().loadImage(this.IMAGES[0]);
     this.loadImages(this.IMAGES);
@@ -11,6 +18,11 @@ class CollectableCoin extends MovableObject {
     this.animate();
   }
 
+  /**
+   * Starts an animation loop that cycles through the coin's images.
+   * Calls `playAnimation` with the `IMAGES` array every 200 milliseconds.
+   * Intended to animate the collectable coin sprite.
+   */
   animate() {
     setInterval(() => {
       this.playAnimation(this.IMAGES);
