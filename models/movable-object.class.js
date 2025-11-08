057d763b71fc2658bd7d36037e5a7a1b373cd4e9
HardavanAlaie <h.alaie@gmx.de>
2025-11-08 11:40:27 +0100

    new version isColliding(mo,... in movable objects class

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -39,16 +39,33 @@ class MovableObject extends DrawableObject {
    * @param {MovableObject} mo - The other object to test collision with.
    * @returns {boolean} True if the objects overlap.
    */
-  isColliding(mo) {
-    if (!mo) return false;
-    const p = 15; // hitbox padding
-    return (
-      this.x + this.width > mo.x - p &&
-      this.x - p < mo.x + mo.width + p &&
-      this.y + this.height > mo.y - p &&
-      this.y - p < mo.y + mo.height + p
-    );
-  }
+  // isColliding(mo) {
+  //   if (!mo) return false;
+  //   const p = 15; // hitbox padding
+  //   return (
+  //     this.x + this.width > mo.x - p &&
+  //     this.x - p < mo.x + mo.width + p &&
+  //     this.y + this.height > mo.y - p &&
+  //     this.y - p < mo.y + mo.height + p
+  //   );
+  // }
+  /** 🔒 Tight AABB collision – requires closer contact (inset shrinks boxes) */
+isCollidingTight(mo, inset = 10) {
+  if (!mo) return false;
+
+  const ax1 = this.x + inset;
+  const ay1 = this.y + inset;
+  const ax2 = this.x + this.width - inset;
+  const ay2 = this.y + this.height - inset;
+
+  const bx1 = mo.x + inset;
+  const by1 = mo.y + inset;
+  const bx2 = mo.x + mo.width - inset;
+  const by2 = mo.y + mo.height - inset;
+
+  return ax2 > bx1 && ax1 < bx2 && ay2 > by1 && ay1 < by2;
+}
+
 
   /**
    * ❤️ Applies damage to the object.
